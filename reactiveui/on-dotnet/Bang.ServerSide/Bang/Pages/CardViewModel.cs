4385dc7abbf7b98f37da2a2e42273aef831bcfbb
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2021-06-23 20:19:38 -0500

    sending signals to signalR

--- reactiveui/on-dotnet/Bang.ServerSide/Bang/Pages/CardViewModel.cs
+++ reactiveui/on-dotnet/Bang.ServerSide/Bang/Pages/CardViewModel.cs
@@ -12,7 +12,8 @@ namespace Bang.Pages
 
         public CardViewModel()
         {
-            var hubConnection = new HubConnectionBuilder().WithUrl("https://functionapp-210623013109.azurewebsites.net/api").Build();
+            // var hubConnection = new HubConnectionBuilder().WithUrl("https://functionapp-210623013109.azurewebsites.net/api").Build();
+            var hubConnection = new HubConnectionBuilder().WithUrl("http://localhost:7071/api").Build();
             hubConnection.On<NewsModel>("newsStream", newsModel =>
                 {
                     News = newsModel;
